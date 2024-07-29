6198bd84d901b6d052af5c5f1c7847d7194c5469
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-30 00:14:02 +0530

    user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateUserDto } from './dto/user.dto';
+import { hash } from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(dto: CreateUserDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (user) {
+      throw new ConflictException('User already exists');
+    }
+
+    const newUser = await this.prisma.user.create({
+      data: { ...dto, password: await hash(dto.password, 10) },
+    });
+    delete newUser.password;
+    return newUser;
+  }
+}
