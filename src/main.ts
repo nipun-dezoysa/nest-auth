792a6070843daee3dd2e34f6399e753be5fbad2e
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-30 16:22:17 +0530

    user profile api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove non-whitelisted properties
+      forbidNonWhitelisted: true, // throw error if there is any non-whitelisted property
+      transform: true, // transform incoming data to DTO
+    }),
+  );
+
   await app.listen(8000);
 }
 bootstrap();
